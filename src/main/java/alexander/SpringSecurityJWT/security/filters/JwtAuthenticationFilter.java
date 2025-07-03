77b3df58464eed7afb8177d1ad5e73dfe075dcf2
Kevin Alexander Sánchez Torres <69257228+alexanderBennington@users.noreply.github.com>
2025-07-03 21:24:20 +0000

    Ajuste de JWT

---
+++ src/main/java/alexander/SpringSecurityJWT/security/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,78 @@
+package alexander.SpringSecurityJWT.security.filters;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import alexander.SpringSecurityJWT.models.UserEntity;
+import alexander.SpringSecurityJWT.security.jwt.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+    private JwtUtils jwtUtils;
+
+    public JwtAuthenticationFilter(JwtUtils jwtUtils){
+        this.jwtUtils = jwtUtils;
+    }
+    
+    @Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+        UserEntity userEntity = null;
+        String username = "";
+        String password = "";
+
+        try {
+            userEntity = new ObjectMapper().readValue(request.getInputStream(), UserEntity.class);
+            username = userEntity.getUsername();
+            password = userEntity.getPassword();
+        } catch (StreamReadException e) {
+            throw new RuntimeException();
+        } catch (DatabindException e){
+            throw new RuntimeException();
+        } catch (IOException e){
+            throw new RuntimeException();
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = 
+            new UsernamePasswordAuthenticationToken(username, password);
+
+		return getAuthenticationManager().authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+
+        User user = (User) authResult.getPrincipal();
+        String token = jwtUtils.generateAccessToken(user.getUsername());
+        response.addHeader("Authorization", token);
+        Map<String, Object> httpResponse = new HashMap<>();
+        httpResponse.put("token", token);
+        httpResponse.put("Message", "Autenticación Correcta");
+        httpResponse.put("username", user.getUsername());
+        response.getWriter().write(new ObjectMapper().writeValueAsString(httpResponse));
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(org.springframework.http.MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().flush();
+
+		super.successfulAuthentication(request, response, chain, authResult);
+    }
+
+}
