ee872ab8f9d772d207680a7f4770cf7c86000f24
Kevin Alexander Sánchez Torres <69257228+alexanderBennington@users.noreply.github.com>
2025-07-03 19:22:36 +0000

    JWT

--- src/main/java/alexander/SpringSecurityJWT/security/jwt/JwtUtils.java
+++ src/main/java/alexander/SpringSecurityJWT/security/jwt/JwtUtils.java
@@ -4,15 +4,25 @@ import java.security.Key;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.SecretKey;
 
 @Component
+@Slf4j
 public class JwtUtils {
 
     @Value("${jwt.secret.key}")
@@ -31,6 +41,45 @@ public class JwtUtils {
                 .expiration(Date.from(expirationInstant))
                 .signWith(getSignatureKey())
                 .compact();
+    }
+
+    public boolean isTokenValid(String token){
+        try{
+            Jwts.parser()
+                .verifyWith((javax.crypto.SecretKey) getSignatureKey())
+                .build()
+                .parse(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty: {}", e.getMessage());
+        } catch (Exception e) {
+            log.error("An unexpected error occurred during token validation: {}", e.getMessage(), e);
+        }
+        return false;
+    }
+
+    public String getUsernameFromToken(String token){
+        return getClaim(token, Claims::getSubject);
+    }
+
+    public <T>T getClaim(String token, Function<Claims, T> claimsFunction){
+        Claims claims = extractAllClaims(token);
+        return claimsFunction.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token){
+        Jws<Claims> jwsClaims = (Jws<Claims>) Jwts.parser()
+                .verifyWith((SecretKey) getSignatureKey())
+                .build()
+                .parse(token);
+
+        return jwsClaims.getPayload(); 
     }
 
     public Key getSignatureKey(){
